075a07c9e789553c106a60206bda43d909137c64
Aman Srivastav <shinchanmusic24@gmail.com>
2023-10-17 15:29:50 +0530

    create auth.js for appwrie at production level

---
+++ src/appwrite/auth.js
@@ -0,0 +1,50 @@
+import conf from "../config/conf.js";
+import { Client, Account, ID } from "appwrite";
+
+
+
+export class AuthService{
+    Client = new Client();
+    account;
+    constructor(){
+        this.Client.setEndpoint(conf.appwriteUrl).setProject(conf.appwriteProjectId);
+        this.account = new Account(this.Client)
+    }
+    async createAccount({email,password,name}){
+        try {
+            const userAccount = await this.account.create(ID.unique(),email,password,name);
+            if(userAccount){
+                return this.login({email,password});
+            }else {
+            return userAccount;
+            }
+        } catch(error){
+            throw error;
+        }
+       
+    }
+    async login({email,password}){
+        try{
+            return await this.account.createEmailSession(email.password);
+        }catch(error){
+            throw error
+        }
+    }
+    async getCurrentUser(){
+        try{
+            return await this.account.get();
+        }catch(error){
+            console.log("Appwrite Error",error);
+        }
+    }
+    async logout(){
+        try {
+            await this.account.deleteSession();
+        } catch (error) {
+            console.log("AppWrite Error" , error)
+        }
+    }
+}
+ const authService = new AuthService();
+export default AuthService
+
