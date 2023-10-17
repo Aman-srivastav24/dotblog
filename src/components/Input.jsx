b0448730b4a0cfbd5e78c14559dd3fdbd3a40414
Aman Srivastav <shinchanmusic24@gmail.com>
2023-10-17 20:17:57 +0530

    created button input components toolkit

---
+++ src/components/Input.jsx
@@ -0,0 +1,23 @@
+import React ,{useId}from 'react'
+
+const Input = React.forwardRef(function Input({
+  label,
+  type = "type",
+  className ="",
+  ...props
+}, ref){
+  const id = useId()
+  return (
+    <div className='w-full'>
+      {label && <lable className='inline-block mb-1 pl-1' htmlFor={props.id}> {label}</lable>}
+      <input type={type}
+      className={`px-3 py rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full 
+      ${className}`}
+      ref={ref}
+      {...props}
+      id={id}
+      />
+    </div>
+  )
+})
+export default Input
--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -7,12 +7,12 @@ const initialState = {
 const authSlice = createSlice({
     name:"auth",
     initialState,
-    reducers:{
-        login:(state,action)=>{
+    reducers: {
+        login: (state,action) => {
             state.status=true;
             state.userData = action.payload.userData;
         },
-        logout:(state,action)=>{
+        logout: (state,action) => {
         state.status = false;
         state.userData =null;
         }
